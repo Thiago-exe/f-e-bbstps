523dcba9e222865903de80313d51b7d52ea96d73
thiago-exe <thiagobriitto@outlook.com>
2024-02-27 17:50:03 -0300

    preloader

--- portfolio/main.js
+++ portfolio/main.js
@@ -1,5 +1,13 @@
 var btnContact = document.querySelector(".jl-btn-contact");
 
+window.addEventListener('load', function(){
+  var pagePreloader = document.querySelector('.jl-preloader');
+  this.setTimeout(function(){
+    pagePreloader.classList.add('jl-fade-out');
+  }, 3000);
+});
+
+
 btnContact.addEventListener("click", function () {
   var boxContact = document.querySelector(".jl-contact-info");
   boxContact.classList.toggle("jl-is-open");
